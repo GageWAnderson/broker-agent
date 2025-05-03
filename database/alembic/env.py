fa89af3da34a26d68156ff68b7f9e3d4d0e99d07
Gage Anderson <gage@gageanderson.io>
2025-05-03 10:24:45 -0500

    docs: updated arch with todo for app layer

--- database/alembic/env.py
+++ database/alembic/env.py
@@ -3,6 +3,8 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from database.models import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -12,11 +14,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata  # Set the target metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -62,9 +60,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
---
+++ database/models.py
@@ -0,0 +1,7 @@
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+# TODO: Create ERD for apartment listings
